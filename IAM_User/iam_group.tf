7f85579007c2352d44488fa6fe4e52d37df56729
basil9242 <105882549+basil9242@users.noreply.github.com>
2024-07-02 20:00:03 +0530

    iam_user_group

--- IAM_User/iam_group.tf
+++ IAM_User/iam_group.tf
@@ -10,6 +10,7 @@ resource "aws_iam_policy" "iam_policy-group" {
 }
 
 resource "aws_iam_group_policy_attachment" "iam_policy_attach_group" {
+    count = var.iam_group_yes_no ? 0:1
     group = aws_iam_group.iam_group.name
     policy_arn = var.iam_policy_json_file ? 1 [aws_iam_policy.iam_policy_group.arn] : [var.policy_arn]
 }
